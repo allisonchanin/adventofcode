29bbbbb8ca58af9baf89e115e4306ba06eade418
Allison Chanin <allison.chanin@gmail.com>
2021-12-19 18:01:39 -0800

    solution to day 2 part 2

---
+++ 2021/day2/aoc_day2p2.py
@@ -0,0 +1,26 @@
+# Allison Chanin
+# advent of code day #2
+
+def main():
+	with open ("positionchanges.txt") as file:
+		commands = file.readlines()
+		depth, horizontal_position = movements(commands)
+		print (depth * horizontal_position)
+
+def movements(position_changes):
+	depth = 0
+	horizontal_position = 0
+	aim = 0
+	for command in position_changes:
+		command = command.rstrip(" \n")
+		if command[:7] == "forward":
+			horizontal_position += int(command[-1])
+			depth += (aim * int(command[-1]))
+		elif command[:4] == "down":
+			aim += int(command[-1])
+		elif command[:2] == "up":
+			aim -= int(command[-1])
+	return depth, horizontal_position
+
+main()
+
